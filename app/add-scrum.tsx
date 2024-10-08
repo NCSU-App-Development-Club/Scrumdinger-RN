0e3b67f350699c35a57f2f8b6b6e50eef4047a4b
EricApostal <ehapostal04@gmail.com>
2024-10-08 19:38:59 -0400

    basic gui implemented
    Co-authored-by: Mordecai Mengesteab <MordecaiM24@users.noreply.github.com>

--- app/add-scrum.tsx
+++ app/add-scrum.tsx
@@ -1,32 +1,109 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Button, Pressable } from 'react-native';
 import { Link } from 'expo-router';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+
+interface TextFieldProps {
+  label: string;
+  hinttext: string;
+}
 
 export default function About() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Add Scrum Page</Text>
-      <Link style={styles.link} href="/"> {"< "}Back</Link>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <>
+      <View style={styles.titleView}>
+        <Link style={styles.link} href="/">
+          <Icon name="arrow-back" size={30} color="#000" />
+        </Link>
+        <Text style={styles.titleText}>Daily Scrums</Text>
+      </View>
+      <SafeAreaView style={styles.container}>
+
+        <TextField label="Meeting Name:" hinttext="Default" />
+        <TextField label="Meeting Length (minutes):" hinttext="Minutes" />
+        <TextField label="Attendees (comma-seperated):" hinttext="Minutes" />
+        <AddScrumButton />
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </>
   );
 }
+
+export function AddScrumButton() {
+  return (
+    <Pressable style={styles.addScrumButton} onPress={() => { }}>
+      <Text style={styles.addScrumButtonText}>Add Scrum</Text>
+    </Pressable>
+  )
+}
+
+export function TextField({ label, hinttext }: TextFieldProps): JSX.Element {
+  return (
+    <View style={styles.textView}>
+      <Text style={styles.labelText}>{label}</Text>
+      <TextInput placeholder={hinttext} style={styles.textBox} />
+    </View>
+  );
+};
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-    borderWidth: 4
-}, link: {
+    justifyContent: 'flex-start',
+  },
+  titleView: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    margin: 8,
+    marginBottom: 24,
+    alignContent: 'flex-start'
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: '300',
+    paddingStart: 10,
+  },
+  labelText: {
+    fontSize: 16,
+    paddingBottom: 8
+  },
+  link: {
+  },
+  textBox: {
+    borderColor: '#D3D3D3',
     borderWidth: 1,
-    width: 72,
-    textAlign: 'center',
-    padding: 6,
-    margin: 4,
-    position: "absolute",
-    top: 4,
-    left: 4
-  }
+    paddingVertical: 12,
+    paddingHorizontal: 8,
+    borderRadius: 4,
+    textAlign: 'left',
+  },
+  textView: {
+    minWidth: '80%',
+    paddingBottom: 24,
+  },
+  addScrumButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  addScrumButtonText: {
+    fontSize: 18,
+    lineHeight: 21,
+    fontWeight: '600',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+
+
 });
