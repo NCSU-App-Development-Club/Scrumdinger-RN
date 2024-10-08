948325d9ef9c293b1ad40ebc1d24a13217e1fbb9
ex4dev <31103658+ex4dev@users.noreply.github.com>
2024-10-08 19:16:07 -0400

    Start basic UI

--- app/team.tsx
+++ app/team.tsx
@@ -5,28 +5,63 @@ import { Link } from 'expo-router';
 export default function About() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Team Page</Text>
-      <Link style={styles.link} href="/"> {"< "}Back</Link>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Link style={styles.link} href="/"> {"< "}Back</Link>
+      </View>
+      <Text style={styles.title}>Microservices</Text>
+      <View>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.body}>
+        <View style={styles.meetingLength}>
+          <Text style={styles.bold}>Meeting Length</Text>
+          <Text style={styles.meetingLengthTime}>: 30 minutes</Text>
+        </View>
+        <View style={styles.attendees}>
+          <Text style={styles.bold}>Attendees:</Text>
+          <View style={styles.list}>
+            <Text>{`\u2022 one`}</Text>
+            <Text>{`\u2022 two`}</Text>
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    alignItems: 'flex-start',
+    textAlign: 'left',
+    width: '100%'
+  },
+  title: {
+    fontSize: 30
+  },
+  body: {
+
+  },
+  list: {
+    marginLeft: 8
+  },
+  meetingLength: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  bold: {
+    fontWeight: 'bold'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderWidth: 4
+    // alignItems: 'center',
+    borderWidth: 4,
+    padding: 8
   }, link: {
     borderWidth: 1,
     width: 72,
     textAlign: 'center',
     padding: 6,
     margin: 4,
-    position: "absolute",
-    top: 4,
-    left: 4
   }
 });
